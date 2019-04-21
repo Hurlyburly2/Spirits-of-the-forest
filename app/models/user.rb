770d8dcd732a43d792a727046384894f093068bb
Hurlyburly2 <dougdougmann@gmail.com>
2019-04-20 22:49:37 -0400

    tests for User model written

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, length: { minimum: 2, maximum: 15 }, uniqueness: true
+  # email seems to be automatically validated
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
