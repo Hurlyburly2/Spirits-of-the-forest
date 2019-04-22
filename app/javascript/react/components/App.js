f552f94be71b3a44ca92d8742bf7b9d026745792
Hurlyburly2 <dougdougmann@gmail.com>
2019-04-22 07:49:55 -0400

    Got React Router set up correctly!

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+import MyGamesContainer from '../containers/MyGamesContainer'
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return(
+    <Router history={browserHistory}>
+      <Route path='/' component={MyGamesContainer} />
+    </Router>
+  )
 }
 
 export default App
+
+// return (<h1>Make It So React</h1>)
