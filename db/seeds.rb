909437b81ddd61547ead2ad742f478caf9330bef
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-08 13:55:31 -0400

    commented back in seeder file so i dont get confused later

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,116 +1,116 @@
-# #BRANCH 6 CARDS
-# 
-# Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/branch-1-8-x1.png")
-# 
-# Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/branch-1m-8-x1.png")
-# 
-# Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/branch-1s-8-x1.png")
-# 
-# Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/branch-1w-8-x1.png")
-# 
-# Card.create(spirit: "branch", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/branch-2-8-x2.png")
-# 
-# Card.create(spirit: "branch", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/branch-2-8-x2.png")
-# 
-# #DEW 5 CARDS
-# 
-# Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "moon", image_url: "/cards/dew-1m-7-x1.png")
-# 
-# Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "sun", image_url: "/cards/dew-1s-7-x1.png")
-# 
-# Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "wind", image_url: "/cards/dew-1w-7-x1.png")
-# 
-# Card.create(spirit: "dew", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/dew-2-7-x2.png")
-# 
-# Card.create(spirit: "dew", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/dew-2-7-x2.png")
-# 
-# #FLOWERS 4 CARDS
-# 
-# Card.create(spirit: "flower", spirit_points: 1, spirit_count: 6, element: "moon", image_url: "/cards/flowers-1m-6-x1.png")
-# 
-# Card.create(spirit: "flower", spirit_points: 1, spirit_count: 6, element: "sun", image_url: "/cards/flowers-1s-6-x1.png")
-# 
-# Card.create(spirit: "flower", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/flowers-2-6-x2.png")
-# 
-# Card.create(spirit: "flower", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/flowers-2-6-x2.png")
-# 
-# #FRUITS 5 CARDS
-# 
-# Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "moon", image_url: "/cards/fruits-1m-6-x1.png")
-# 
-# Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "sun", image_url: "/cards/fruits-1s-6-x1.png")
-# 
-# Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "wind", image_url: "/cards/fruits-1w-6-x2.png")
-# 
-# Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "wind", image_url: "/cards/fruits-1w-6-x2.png")
-# 
-# Card.create(spirit: "fruit", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/fruits-2-6-1x.png")
-# 
-# #LEAVES 6 CARDS
-# 
-# Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/leaves-1-8-x1.png")
-# 
-# Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/leaves-1m-8-x1.png")
-# 
-# Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/leaves-1s-8-x1.png")
-# 
-# Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/leaves-1w-8-x1.png")
-# 
-# Card.create(spirit: "leaf", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/leaves-2-8-x2.png")
-# 
-# Card.create(spirit: "leaf", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/leaves-2-8-x2.png")
-# 
-# #MOSS 4 CARDS
-# 
-# Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "moon", image_url: "/cards/moss-1m-5-x1.png")
-# 
-# Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "sun", image_url: "/cards/moss-1s-5-x1.png")
-# 
-# Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "wind", image_url: "/cards/moss-1w-5-x1.png")
-# 
-# Card.create(spirit: "moss", spirit_points: 2, spirit_count: 5, element: "none", image_url: "/cards/moss-2-5-x1.png")
-# 
-# #MUSHROOM 5 CARDS
-# 
-# Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "moon", image_url: "/cards/mushroom-1m-7-x1.png")
-# 
-# Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "sun", image_url: "/cards/mushroom-1s-7-x1.png")
-# 
-# Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "wind", image_url: "/cards/mushroom-1w-7-x1.png")
-# 
-# Card.create(spirit: "mushroom", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/mushroom-2-7-x2.png")
-# 
-# Card.create(spirit: "mushroom", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/mushroom-2-7-x2.png")
-# 
-# #SPIDER 7 CARDS
-# 
-# Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "none", image_url: "/cards/spider-1-10-x1.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "moon", image_url: "/cards/spider-1m-10-x1.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "sun", image_url: "/cards/spider-1s-10-x1.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "wind", image_url: "/cards/spider-1w-10-x1.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
-# 
-# Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
-# 
-# #VINE 6 CARDS
-# 
-# Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/vines-1-8-x1.png")
-# 
-# Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/vines-1m-8-x1.png")
-# 
-# Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/vines-1s-8-x1.png")
-# 
-# Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/vines-1w-8-x1.png")
-# 
-# Card.create(spirit: "vine", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/vines-2-8-x2.png")
-# 
-# Card.create(spirit: "vine", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/vines-2-8-x2.png")
+#BRANCH 6 CARDS
+
+Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/branch-1-8-x1.png")
+
+Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/branch-1m-8-x1.png")
+
+Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/branch-1s-8-x1.png")
+
+Card.create(spirit: "branch", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/branch-1w-8-x1.png")
+
+Card.create(spirit: "branch", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/branch-2-8-x2.png")
+
+Card.create(spirit: "branch", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/branch-2-8-x2.png")
+
+#DEW 5 CARDS
+
+Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "moon", image_url: "/cards/dew-1m-7-x1.png")
+
+Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "sun", image_url: "/cards/dew-1s-7-x1.png")
+
+Card.create(spirit: "dew", spirit_points: 1, spirit_count: 7, element: "wind", image_url: "/cards/dew-1w-7-x1.png")
+
+Card.create(spirit: "dew", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/dew-2-7-x2.png")
+
+Card.create(spirit: "dew", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/dew-2-7-x2.png")
+
+#FLOWERS 4 CARDS
+
+Card.create(spirit: "flower", spirit_points: 1, spirit_count: 6, element: "moon", image_url: "/cards/flowers-1m-6-x1.png")
+
+Card.create(spirit: "flower", spirit_points: 1, spirit_count: 6, element: "sun", image_url: "/cards/flowers-1s-6-x1.png")
+
+Card.create(spirit: "flower", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/flowers-2-6-x2.png")
+
+Card.create(spirit: "flower", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/flowers-2-6-x2.png")
+
+#FRUITS 5 CARDS
+
+Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "moon", image_url: "/cards/fruits-1m-6-x1.png")
+
+Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "sun", image_url: "/cards/fruits-1s-6-x1.png")
+
+Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "wind", image_url: "/cards/fruits-1w-6-x2.png")
+
+Card.create(spirit: "fruit", spirit_points: 1, spirit_count: 6, element: "wind", image_url: "/cards/fruits-1w-6-x2.png")
+
+Card.create(spirit: "fruit", spirit_points: 2, spirit_count: 6, element: "none", image_url: "/cards/fruits-2-6-1x.png")
+
+#LEAVES 6 CARDS
+
+Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/leaves-1-8-x1.png")
+
+Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/leaves-1m-8-x1.png")
+
+Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/leaves-1s-8-x1.png")
+
+Card.create(spirit: "leaf", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/leaves-1w-8-x1.png")
+
+Card.create(spirit: "leaf", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/leaves-2-8-x2.png")
+
+Card.create(spirit: "leaf", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/leaves-2-8-x2.png")
+
+#MOSS 4 CARDS
+
+Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "moon", image_url: "/cards/moss-1m-5-x1.png")
+
+Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "sun", image_url: "/cards/moss-1s-5-x1.png")
+
+Card.create(spirit: "moss", spirit_points: 1, spirit_count: 5, element: "wind", image_url: "/cards/moss-1w-5-x1.png")
+
+Card.create(spirit: "moss", spirit_points: 2, spirit_count: 5, element: "none", image_url: "/cards/moss-2-5-x1.png")
+
+#MUSHROOM 5 CARDS
+
+Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "moon", image_url: "/cards/mushroom-1m-7-x1.png")
+
+Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "sun", image_url: "/cards/mushroom-1s-7-x1.png")
+
+Card.create(spirit: "mushroom", spirit_points: 1, spirit_count: 7, element: "wind", image_url: "/cards/mushroom-1w-7-x1.png")
+
+Card.create(spirit: "mushroom", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/mushroom-2-7-x2.png")
+
+Card.create(spirit: "mushroom", spirit_points: 2, spirit_count: 7, element: "none", image_url: "/cards/mushroom-2-7-x2.png")
+
+#SPIDER 7 CARDS
+
+Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "none", image_url: "/cards/spider-1-10-x1.png")
+
+Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "moon", image_url: "/cards/spider-1m-10-x1.png")
+
+Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "sun", image_url: "/cards/spider-1s-10-x1.png")
+
+Card.create(spirit: "spider", spirit_points: 1, spirit_count: 10, element: "wind", image_url: "/cards/spider-1w-10-x1.png")
+
+Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
+
+Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
+
+Card.create(spirit: "spider", spirit_points: 2, spirit_count: 10, element: "none", image_url: "/cards/spider-2-10-x3.png")
+
+#VINE 6 CARDS
+
+Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "none", image_url: "/cards/vines-1-8-x1.png")
+
+Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "moon", image_url: "/cards/vines-1m-8-x1.png")
+
+Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "sun", image_url: "/cards/vines-1s-8-x1.png")
+
+Card.create(spirit: "vine", spirit_points: 1, spirit_count: 8, element: "wind", image_url: "/cards/vines-1w-8-x1.png")
+
+Card.create(spirit: "vine", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/vines-2-8-x2.png")
+
+Card.create(spirit: "vine", spirit_points: 2, spirit_count: 8, element: "none", image_url: "/cards/vines-2-8-x2.png")
 
 
 
