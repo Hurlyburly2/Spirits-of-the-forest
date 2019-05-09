84b7139028ca13f67a2aed819842e1a934e79c60
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-08 22:55:33 -0400

    setup for checking for inactive games is completed

--- app/models/appstamp.rb
+++ app/models/appstamp.rb
@@ -1,7 +1,7 @@
 class Appstamp < ApplicationRecord
   def self.check_for_inactive_games
     last_check = Appstamp.first.last_activity_check
-    if (Time.now - last_check) / 1.minute > 5
+    if (Time.now - last_check) / 1.hour > 5
       binding.pry
     else
       binding.pry
