e89b33383c66deb307b96fc0f2f2a3818747dba4
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-09 13:12:39 -0400

    games are checked for inactivity and reminders are sent out after 24 hours to the player whose turn it is. if another 24 hours passes, the player will automatically concede. Reminders can be toggled on and off in the account screen. Games will be deleted after 48 hours of inactivity even if the user has notifications turned off

--- app/models/appstamp.rb
+++ app/models/appstamp.rb
@@ -5,7 +5,7 @@ class Appstamp < ApplicationRecord
   def self.check_for_inactive_games
     last_check = Appstamp.first.last_activity_check
     check_for_idle_duration = 6
-    if (Time.now - last_check) / 1.hour > check_for_idle_duration
+    if (Time.now - last_check) / 1.hours > check_for_idle_duration
       game_idle_duration = 24
       games_to_check = Game.all
       idle_games = []
@@ -15,25 +15,29 @@ class Appstamp < ApplicationRecord
         user = User.find(game.whose_turn_id)
         current_match = game.matches.where(user: user)[0]
         if user.reminders == true
+          binding.pry
           if (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == false
+            binding.pry
             idle_games << game
             current_match.reminded = true
             current_match.save
             #REMINDER
           elsif (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == true
-            if (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == false
+            binding.pry
+            Game.concede_game_timeout(game)
             #CONCEDE GAME IN HERE
-            end
           end
         else
+          binding.pry
           if (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == false
+            binding.pry
             current_match.reminded = true
             current_match.save
             #NO REMINDER, BUT ONE STEP CLOSER TO CONCESSION
           elsif (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == true
-            if (Time.now - game.updated_at) / 1.hour > game_idle_duration && current_match.reminded == false
+            binding.pry
+            Game.concede_game_timeout(game)
             #CONCEDE GAME IN HERE
-            end
           end
         end
       end
