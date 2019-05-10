428b2ebe12f1aa5d1ff60f5c8fa176eef9cac7a4
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-10 14:48:48 -0400

    the gems and gem tokens are dooone

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   
   resources :matches, only: [:index]
   
+  resources :instructions, only: [:index]
+  
   namespace :api do
     namespace :v1 do
       resources :games, only: [:index, :update, :create, :show, :destroy]
