1deb5219db401858b30492cd647f3231fcb9918e
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-10 22:16:59 -0400

    oh my god this is such a better idea

--- app/javascript/react/containers/GameplayContainer.js
+++ app/javascript/react/containers/GameplayContainer.js
@@ -54,7 +54,7 @@ class GameplayContainer extends Component {
   componentDidMount() {
     let backgroundDiv = document.getElementById('overlay') 
     backgroundDiv.classList.add('overlay')
-    let refreshInterval = 2000 
+    let refreshInterval = 200000000 
     this.refreshInterval = setInterval(() => this.getGameData(), refreshInterval);
     this.getGameData();
   }
