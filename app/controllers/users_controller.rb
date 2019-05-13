4cbef72cd2ad69aa3d8b69024f9e1fca1f270947
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-13 16:25:59 -0400

    styled account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
     @progress_bar_style = ""
     if current_user.rankup_score >= 0 && current_user.rankup_score < 100
       rank_up_percentage = current_user.rankup_score
-      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#CD7F32;"
+      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#CD7F32;border-width:1px;height:28px"
     elsif current_user.rankup_score >= 100 && current_user.rankup_score < 200
       rank_up_percentage = current_user.rankup_score - 100
-      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#C0C0C0;"
+      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#C0C0C0;border-width:1px;height:28px"
     elsif current_user.rankup_score >= 200 && current_user.rankup_score < 300
       rank_up_percentage = current_user.rankup_score - 200
-      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#FFD700;"
+      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#FFD700;border-width:1px;height:28px"
     elsif current_user.rankup_score >= 300 && current_user.rankup_score < 400
       rank_up_percentage = current_user.rankup_score - 300
-      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#B9F2FF;"
+      @progress_bar_style = "width:#{rank_up_percentage}%;background-color:#B9F2FF;border-width:1px;height:28px"
     else
       @progress_bar_style = "width:100%;background-color:#ff3276;"
     end
@@ -41,6 +41,7 @@ class UsersController < ApplicationController
         "Vine",
         "Wind"
       ]
+      @default_pic = @profile_image_collection[current_user.which_profile_pic]
     
   end
   
@@ -54,35 +55,48 @@ class UsersController < ApplicationController
       flash.now[:sad] = "Notifications Turned On"
     end
     
-    profile_pic_num = 1
+    @profile_pic_num = 1
+    @default_pic = ""
     case params["user"]["which_profile_pic"]
       when "Branch"
-        profile_pic_num = 1
+        @profile_pic_num = 1
+        @default_pic = "Branch"
       when "Dew"
-        profile_pic_num = 2
+        @profile_pic_num = 2
+        @default_pic = "Dew"
       when "Flower"
-        profile_pic_num = 3
+        @profile_pic_num = 3
+        @default_pic = "Flower"
       when "Fruit"
-        profile_pic_num = 4
+        @profile_pic_num = 4
+        @default_pic = "Fruit"
       when "Moss"
-        profile_pic_num = 5
+        @profile_pic_num = 5
+        @default_pic = "Moss"
       when "Mushroom"
-        profile_pic_num = 6
+        @profile_pic_num = 6
+        @default_pic = "Mushroom"
       when "Moon"
-        profile_pic_num = 7
+        @profile_pic_num = 7
+        @default_pic = "Moon"
       when "Spider"
-        profile_pic_num = 8
+        @profile_pic_num = 8
+        @default_pic = "Spider"
       when "Vine"
-        profile_pic_num = 9
+        @profile_pic_num = 9
+        @default_pic = "Vine"
       when "Leaf"
-        profile_pic_num = 10
+        @profile_pic_num = 10
+        @default_pic = "Leaf"
       when "Sun"
-        profile_pic_num = 11
+        @profile_pic_num = 11
+        @default_pic = "Sun"
       when "Wind"
-        profile_pic_num = 12
+        @profile_pic_num = 12
+        @default_pic = "Wind"
     end
 
-    current_user.which_profile_pic = profile_pic_num
+    current_user.which_profile_pic = @profile_pic_num
     current_user.reminders = update_email_value
     current_user.save
     
