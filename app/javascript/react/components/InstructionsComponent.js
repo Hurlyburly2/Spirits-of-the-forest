5d843515617a400a302a850e803838b33fed2465
Hurlyburly2 <dougdougmann@gmail.com>
2019-05-15 15:08:35 -0400

    all content for instructions is added (no styling yet)

--- app/javascript/react/components/InstructionsComponent.js
+++ app/javascript/react/components/InstructionsComponent.js
@@ -4,7 +4,19 @@ import { Link } from 'react-router';
 const InstructionsComponent = (props) => {
   
   return(
-    <h1>Instructions Component</h1>
+    <div>
+      <img src="/misc/SOTF-white-logo.png" />
+      <div>
+        <img src={props.image} />
+      </div>
+      <div>
+        {props.content}
+      </div>
+      <div>
+        <img src={props.leftArrow} onClick={props.leftArrowAction} />
+        <img src={props.rightArrow} onClick={props.rightArrowAction} />
+      </div>
+    </div>
   )
 }
 
